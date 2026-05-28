5b6d632083404d123c51586f0feaac527cff2174
Emilli-hub <emillisena557@gmail.com>
2026-05-28 15:30:00 -0300

    Add files via upload
    Projeto de controle de vendas desenvolvido em Python utilizando pandas. O sistema realiza cálculos automáticos de total, desconto e valor final, além de exportar relatórios em Excel.

--- Projetos.py
+++ Projetos.py
@@ -1,9 +1,19 @@
 import pandas as pd
 dados = {
-    "NOME":["Ana", "João", "Maria"],
-    "SALARIO":[2000, 3000, 4000]
+    "PRODUTO":["Notebook", "Mouse", "Teclado", "Monitor"],
+    "QUANTIDADE":[2, 5, 3, 1],
+    "PRECO":[3000, 100, 150, 1200]
 }
 tabela = pd.DataFrame(dados)
-tabela["BONUS"] = tabela["SALARIO"] * 0.10
-tabela["STATUS"] = ["BONUS NORMAL", "BONUS ALTO", "BONUS ALTO"]
+tabela["TOTAL"] = tabela["QUANTIDADE"] * tabela["PRECO"]
+desconto = []
+for valor in tabela["TOTAL"]:
+    if valor >= 1000:
+       desconto.append(valor * 0.10)
+    else:
+       desconto.append(0)
+tabela["DESCONTO"] = desconto
+tabela["VALOR FINAL"] = tabela["TOTAL"] - tabela["DESCONTO"]
+tabela.to_excel("controle_vendas.xlsx", index=False)
+print("Excel criado com sucesso!")
 print(tabela)
